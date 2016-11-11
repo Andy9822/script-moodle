45752bad7737bf3e948b196a034f38c7111c44a4
vianna274 <leonardo.vianna.f@gmail.com>
2016-11-11 17:08:27 -0200

    Pula primeira linha e converte pra datetime

--- Function.py
+++ Function.py
@@ -22,24 +22,25 @@ def rangerows(arq_xls):
     plan = xls.sheets()[0]
     return plan.nrows
 
-def how_many_post(log,name):
+def how_many_visua(log,name):
     # Retorna o numero de "aparições" de "a"
     a=0
     i=0
     # Verifica linha por linha se tem alguém com o mesmo nome, se tiver acrescenta no valor a ser devolvido
-    for linha in xlread(arquivo):
+    for linha in log:
         if log[i].name == name:
             a=a+1
         i=i+1
     return a
 
-def create_dic(log):
+
+def create_dic_name_number(log):
     # Cria um dicionario com "Nome da pessoa" : "Quantos visualizaçoes"
     dic={}
     i=0
-    for linha in xlread(arquivo):
+    for linha in log:
         if not log[i].name in dic:
-            dic[log[i].name] = how_many_post(log,log[i].name)
+            dic[log[i].name] = how_many_visua(log,log[i].name)
         i=i+1
     return dic
 
@@ -50,17 +51,38 @@ def present_people(dic):
         ppl.append(key)
     return ppl
 
-def sorted_pseud_dic(dic):
+def sorted_number_dic(dic):
     # Recebe o dicionary e retorna 2 listas people and number
     import operator
     people=[]
     number=[]
     # Passa o dicionary para uma lista de tuplas e ordenada essa lista
     sorted_ = sorted(dic.items(), key = operator.itemgetter(1))
-
     people,number=zip(*sorted_)
     return people,number
 
+def plotgraph(people,number):
+    import matplotlib.pyplot as plt
+    plt.rcdefaults()
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # Peoples e Number já estão atualizados e ordenados
+    y_pos = np.arange(len(people))
+    # Number = os numeros de cada people
+    error = np.random.rand(len(people))
+    plt.barh(y_pos, number,0.8, align='center', alpha=0.5)
+    plt.yticks(y_pos, people)
+    plt.xlabel('Perguntas')
+    plt.title('Quantia de visitas ao fórum')
+
+    plt.show()
+
+def convert_to_datetime(log):
+    from datetime import datetime
+    for x in log:
+        date_object = datetime.strptime(log[x].date,"%d/%m/%Y %H:%M")
+    return date_object
 # You are a CHAMPION BRO
 class Champion:
     def __init__(self, date, name, affected, component, context, event, description, source, ip):
@@ -88,30 +110,22 @@ else:
     # Inicia a lista
     log={}
     i=0
-    # For para inserir as 9 categorias de cada um na lista
+    # Faz um for ignorando a primeira linha
     for linha in xlread(arquivo):
+        if not linha[0] == "Hora":
             # Carrega as 9 características em 1 e vai para o próximo
             log[i] = Champion(linha[0],linha[1],linha[2],linha[3],linha[4],linha[5],linha[6],linha[7],linha[8])
-            print (log[i].name)
             i=1+i
+    dates=convert_to_datetime(log)
+    print (dates)
     print ("Carregamento concluido")
     # Cria o dicionary
-    dic=create_dic(log)
+    dic=create_dic_name_number(log)
     # Inicializa as variaveis para ordenar o dicionary
-    people,number=sorted_pseud_dic(dic)
+    people,number=sorted_number_dic(dic)
+    # Plota o grafico
+    plotgraph(people,number)
+
+
 
 #--------------------------------------------- PLOT GRAPH ------------------------------------#
-import matplotlib.pyplot as plt
-plt.rcdefaults()
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Peoples e Number já estão atualizados e ordenados
-y_pos = np.arange(len(people))
-# Number = os numeros de cada people 
-plt.barh(y_pos, number, align='center', alpha=0.5)
-plt.yticks(y_pos, people)
-plt.xlabel('Perguntas')
-plt.title('Quantia de visitas ao fórum')
-
-plt.show()
